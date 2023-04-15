21cb966a6dc4ced7131ef3ad22a1bd999dc2533a
Bluzzi <camilledugas.pro@gmail.com>
2023-04-15 04:47:12 +0200

    feat(button.tsx): add support for button variants (primary, secondary, danger)

--- src/libs/components/button.tsx
+++ src/libs/components/button.tsx
@@ -1,11 +1,23 @@
 import { ButtonHTMLAttributes, ReactElement } from "react";
 import { clsx } from "clsx";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "primary" | "secondary" | "danger";
+};
 
-export function Button({ children, className, ...props }: ButtonProps): ReactElement {
+export function Button({ variant = "primary", children, className, ...props }: ButtonProps): ReactElement {
+  const styles = clsx(
+    "text-white rounded py-2 px-5",
+    {
+      "bg-gray-800 hover:bg-gray-900": variant === "primary",
+      "bg-gray-500 hover:bg-gray-600": variant === "secondary",
+      "bg-red-800 hover:bg-red-900": variant === "danger"
+    },
+    className
+  );
+
   return (
-    <button className={clsx("bg-gray-800 hover:bg-gray-900 text-white rounded py-2 px-5", className)} {...props}>
+    <button className={styles} {...props}>
       {children}
     </button>
   );
