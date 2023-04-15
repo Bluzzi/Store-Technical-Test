57bfbad85c54b19d0e062cdbcf08e5e2efe7450e
Bluzzi <camilledugas.pro@gmail.com>
2023-04-15 04:47:24 +0200

    feat(Cart.tsx): add support for clearing cart and displaying product cards in cart page using react-query

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,30 +1,44 @@
+import { Button } from "#/libs/components/button";
+import { Loading } from "#/libs/components/loading";
+import { ProductCard } from "#/libs/components/product-card";
+import { ProductCart } from "#/libs/components/product-cart";
+import { useCartStore } from "#/libs/stores/cart/cart.store";
+import { IProduct } from "#/libs/types/product";
+import { jsonFetch } from "#/libs/utils/fetch";
+import { useQuery } from "@tanstack/react-query";
 import type { ReactElement } from "react";
-import { useCartContext } from "../libs/contexts/cart";
-import { Link } from "react-router-dom";
 
 export function Cart(): ReactElement {
-  const cart = useCartContext();
+  const products = useCartStore(state => state.products);
+  const clearCart = useCartStore(state => state.clearProducts);
+
+  const { data, status } = useQuery(["/products", products], () => {
+    const promises: Promise<IProduct>[] = [];
+
+    // Bad practice, but the API does not support other method:
+    for (const product of products) promises.push(jsonFetch<IProduct>(`/products/${product.id}`));
+
+    return Promise.all(promises);
+  });
+
+  if (status === "loading") return <Loading />;
+  if (status === "error") return <p>API error</p>;
 
   return (
-    <div>
-      <h1>Cart</h1>
-      <Link to="/products">View products</Link>
-
-      <h2>Products in cart:</h2>
-      {cart.products.map(product => (
-        <div key={product.id}>
-          <p>ID: {product.id}</p>
-          <p>Title: {product.title}</p>
-          <p>Description: {product.description}</p>
-          <p>Price: {product.price}</p>
-          <p>Category: {product.category}</p>
-          <p>Rate: {product.rating.rate}</p>
-
-          <button onClick={() => cart.remove(product)}>Remove this product</button>
-
-          <hr />
-        </div>
+    <div className="flex flex-col items-center gap-5 py-5">
+      {data.map(product => (
+        <ProductCart
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          image={product.image}
+          price={product.price}
+          quantity={products.find(element => element.id === product.id)?.count || 0}
+        />
       ))}
+
+      <Button variant="danger" onClick={() => clearCart()}>Clear cart</Button>
     </div>
   );
 }
