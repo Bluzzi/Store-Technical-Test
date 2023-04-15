c8b6122795120bfe632471d8ae56d198413527ec
Bluzzi <camilledugas.pro@gmail.com>
2023-04-15 03:59:54 +0200

    feat(navbar.tsx): add links to Products and Cart pages in navbar refactor(navbar.tsx): use useCartStore to get product count in cart

--- src/libs/components/navbar.tsx
+++ src/libs/components/navbar.tsx
@@ -1,5 +1,26 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { useCartStore } from "#/libs/stores/cart/cart.store";
 
 export function Navbar(): ReactElement {
-  return <p>Hello world</p>;
+  const productCount = useCartStore(state => state.products.length);
+
+  const links = [
+    { name: "Products", link: "/products" },
+    { name: `Cart (${productCount})`, link: "/products" }
+  ];
+
+  return (
+    <nav className="bg-gray-900 py-4 flex items-center justify-center gap-5">
+      {links.map(element => (
+        <Link
+          key={element.link}
+          to={element.link}
+          className="bg-gray-200 hover:bg-gray-300 rounded py-2 px-5"
+        >
+          {element.name}
+        </Link>
+      ))}
+    </nav>
+  );
 }
