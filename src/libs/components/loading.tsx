d4d72d0aa840e648f9507ada74ad9fb9e1ce702d
Bluzzi <camilledugas.pro@gmail.com>
2023-04-15 04:17:12 +0200

    style(loading.tsx): center loading spinner using margin auto on x and y axis

--- src/libs/components/loading.tsx
+++ src/libs/components/loading.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from "react";
 
 export function Loading(): ReactElement {
-  return <img src="/loading.svg" alt="loading" className="block h-48 w-48" />;
+  return <img src="/loading.svg" alt="loading" className="block h-48 w-48 mx-auto my-5" />;
 }
