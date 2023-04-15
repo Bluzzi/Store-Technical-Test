bb40b814b758308460ea6ea61c6ee76880efd39f
Bluzzi <camilledugas.pro@gmail.com>
2023-04-15 04:27:33 +0200

    refactor(product-detail.tsx): replace useCartContext with useCartStore feat(product-detail.tsx): add Loading and Button components feat(product-detail.tsx): add product details to the page and update addToCart function to use product id instead of product object

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -3,35 +3,34 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { jsonFetch } from "#/libs/utils/fetch";
 import { IProduct } from "#/libs/types/product";
-import { useCartContext } from "#/libs/contexts/cart";
+import { useCartStore } from "#/libs/stores/cart/cart.store";
+import { Loading } from "#/libs/components/loading";
+import { Button } from "#/libs/components/button";
 
 export function ProductDetail(): ReactElement {
   const { productId } = useParams();
-  const cart = useCartContext();
+  const incProduct = useCartStore(state => state.incProduct);
   const navigate = useNavigate();
 
   const { data, status } = useQuery(["/products", productId], () => jsonFetch<IProduct>(`/products/${productId}`));
 
-  const addToCart = (product: IProduct): void => {
-    cart.add(product);
+  const addToCart = (productId: number): void => {
+    incProduct(productId);
     navigate("/cart");
   };
 
-  if (status === "loading") return <p>Loading...</p>;
+  if (status === "loading") return <Loading />;
   if (status === "error") return <p>API error</p>;
 
   return (
-    <div>
-      <h1>Product {data.title} ({data.id})</h1>
+    <div className="bg-gray-300 rounded w-11/12 max-w-4xl mx-auto my-5 p-5 flex flex-col gap-3">
+      <p className="font-bold text-xl">Title: {data.title}</p>
+      <p>{data.description}</p>
+      <p><span className="font-bold">Price:</span> {data.price}</p>
+      <p><span className="font-bold">Category:</span> {data.category}</p>
+      <p><span className="font-bold">Rating:</span> {data.rating.rate}/5</p>
 
-      <p>ID: {data.id}</p>
-      <p>Title: {data.title}</p>
-      <p>Description: {data.description}</p>
-      <p>Price: {data.price}</p>
-      <p>Category: {data.category}</p>
-      <p>Rate: {data.rating.rate}</p>
-
-      <button onClick={() => addToCart(data)}>Add to cart</button>
+      <Button onClick={() => addToCart(data.id)}>Add to cart</Button>
     </div>
   );
 }
